7033f8d4f3e73b1deb0f9a16d1351aa67bbde15d
Yazan Hijazi <136443990+yaznhijazii@users.noreply.github.com>
2024-06-30 14:01:36 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,10 +7,16 @@ let index = 0;
 // JavaScript for Mobile Menu Toggle
 const mobileMenuToggle = document.getElementById('mobile-menu-toggle');
 const mobileMenuOverlay = document.getElementById('mobile-menu-overlay');
+const navLinks = document.getElementById('nav-links'); // Desktop navigation links
 
 mobileMenuToggle.addEventListener('click', function() {
+    // Toggle mobile menu overlay visibility
     mobileMenuOverlay.classList.toggle('open');
+
+    // Optional: Hide desktop navigation links when mobile menu is open
+    navLinks.style.display = navLinks.style.display === 'none' ? 'flex' : 'none';
 });
+
 
 
 // Flip Skills Cards
