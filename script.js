6f98affa2b45aded5adbb640facdd80f122933bf
Yazan Hijazi <136443990+yaznhijazii@users.noreply.github.com>
2024-06-30 13:19:13 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const welcomeText = document.querySelector('.hero-title');
 const text = "Welcome! I'm Yazan Hijazi";
 let index = 0;
 
+
+// JavaScript for Mobile Menu Toggle
+const mobileMenuToggle = document.getElementById('mobile-menu-toggle');
+const mobileMenuOverlay = document.getElementById('mobile-menu-overlay');
+
+mobileMenuToggle.addEventListener('click', function() {
+    mobileMenuOverlay.classList.toggle('open');
+});
+
 // Flip Skills Cards
 const skillCards = document.querySelectorAll('.skill-card');
 
